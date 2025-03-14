3f39b03cc3533e2e4f9a59864bc039d8d03d8837
Sahej Agarwal <sahej.a05@gmail.com>
2025-03-14 19:47:14 +0800

    Gradle Fixes

---
+++ src/main/java/seedu/duke/CEGStudyBuddy.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class CEGStudyBuddy {
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to CEG Study Buddy!");
+    }
+}
