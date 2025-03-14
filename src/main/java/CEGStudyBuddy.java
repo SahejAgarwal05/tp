56637f2d4217d7f11ae65ea5fb6387be876dac20
Sahej Agarwal <sahej.a05@gmail.com>
2025-03-14 21:02:05 +0800

    fix

--- src/main/java/CEGStudyBuddy.java
+++ src/main/java/CEGStudyBuddy.java
@@ -1,5 +1,16 @@
 public class CEGStudyBuddy {
     private String name;
+    public CEGStudyBuddy(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to CEG Study Buddy!");
