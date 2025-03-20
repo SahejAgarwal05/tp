2e4563bb4c4c42c95fc237a604c38bdbad47dce8
HightechTR <explosion64.hightechtr@gmail.com>
2025-03-20 15:26:51 +0800

    Add help command

--- src/main/java/studybuddy/CEGStudyBuddy.java
+++ src/main/java/studybuddy/CEGStudyBuddy.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import studybuddy.commands.Command;
-import studybuddy.commands.AddCommand;
+import studybuddy.commands.HelpCommand;
 import studybuddy.commands.InvalidCommand;
 import studybuddy.course.Course;
 
@@ -12,6 +12,18 @@ public class CEGStudyBuddy {
     public static ArrayList<Course> courses = new ArrayList<>();
     public static boolean isRunning = true;
     public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (isRunning) {
+            String[] userInput = readInput();
+            try {
+                Command c = parseCommand(userInput);
+                System.out.println(c.execute());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Missing Parameters");
+            }
+        }
+    }
 
     static void exitProgram() {
         System.out.println("Bye");
@@ -27,22 +39,10 @@ public class CEGStudyBuddy {
     static Command parseCommand(String[] inputParts) throws IndexOutOfBoundsException {
         Command c = null;
         switch (inputParts[0]) {
-        case "add" -> c = new AddCommand(inputParts[1]);
+        case "help" -> c = new HelpCommand(inputParts[0]);
         case "exit" -> exitProgram();
-        default -> c = new InvalidCommand(inputParts[1]);
+        default -> c = new InvalidCommand(inputParts[0]);
         }
         return c;
-    }
-
-    public static void main(String[] args) {
-        while (isRunning) {
-            String[] userInput = readInput();
-            try {
-                Command c = parseCommand(userInput);
-                System.out.println(c.execute());
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Missing Parameters");
-            }
-        }
     }
 }
---
+++ src/main/java/studybuddy/commands/HelpCommand.java
@@ -0,0 +1,25 @@
+package studybuddy.commands;
+
+public class HelpCommand extends Command {
+    public HelpCommand(String param) {
+        super(param);
+    }
+
+    @Override
+    public String execute() {
+        return """
+                List of Commands:
+                add c/CODE t/TITLE mc/MODULAR_CREDITS y/YEAR s/SEMESTER
+                    Adds a course with the given parameters.
+                delete c/CODE
+                    Deletes a course.
+                list
+                    Lists all courses.
+                edit c/CODE [t/TITLE] [mc/MODULAR_CREDITS] [y/YEAR] [s/SEMESTER]
+                    Edits a course with the given parameters.
+                workload
+                    Displays the total workload for each semester.
+                help
+                    Displays this command list.""";
+    }
+}
