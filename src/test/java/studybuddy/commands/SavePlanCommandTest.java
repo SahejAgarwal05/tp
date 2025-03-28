288ccfb319d52c40002244cf015b0d38e534e6ed
Sahej Agarwal <65767409+SahejAgarwal05@users.noreply.github.com>
2025-03-28 18:27:37 +0800

    Update SavePlanCommandTest.java

--- src/test/java/studybuddy/commands/SavePlanCommandTest.java
+++ src/test/java/studybuddy/commands/SavePlanCommandTest.java
@@ -6,6 +6,7 @@ import studybuddy.CEGStudyBuddy;
 import studybuddy.StorageManager;
 import studybuddy.course.CourseList;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 
