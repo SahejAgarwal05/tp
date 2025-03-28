08290352043523c835a703ff923d2f9b10329e1b
syCHEN1645 <e1121133@u.nus.edu>
2025-03-28 17:16:32 +0800

    Minor Change to Pass Style Checks

--- src/test/java/studybuddy/commands/EditCommandTest.java
+++ src/test/java/studybuddy/commands/EditCommandTest.java
@@ -1,7 +1,6 @@
 package studybuddy.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
