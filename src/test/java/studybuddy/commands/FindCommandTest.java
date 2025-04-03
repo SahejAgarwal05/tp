4c5dc07f44fb6ba23c6eacf9bb81f3b8944a226e
Rishi7830 <e1123021@u.nus.edu>
2025-04-03 20:12:58 +0800

    FIX 1

--- src/test/java/studybuddy/commands/FindCommandTest.java
+++ src/test/java/studybuddy/commands/FindCommandTest.java
@@ -7,7 +7,10 @@ import studybuddy.data.course.CourseList;
 import studybuddy.data.exception.CEGStudyBuddyException;
 import studybuddy.data.storage.StorageManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FindCommandTest {
     private CourseList courses;
