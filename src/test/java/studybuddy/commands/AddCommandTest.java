54005cb32d5d68e6ab94977939a6d4e3aad4749c
syCHEN1645 <e1121133@u.nus.edu>
2025-04-06 03:03:21 +0800

    Minor Changes for Style Checks

--- src/test/java/studybuddy/commands/AddCommandTest.java
+++ src/test/java/studybuddy/commands/AddCommandTest.java
@@ -2,8 +2,6 @@ package studybuddy.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Scanner;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import studybuddy.data.course.CourseList;
