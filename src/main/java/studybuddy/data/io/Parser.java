1db0a5b2e68a4a64b46d836e912a35479666e90c
Rishi7830 <e1123021@u.nus.edu>
2025-04-06 18:54:22 +0800

    fixed line lenght

--- src/main/java/studybuddy/data/io/Parser.java
+++ src/main/java/studybuddy/data/io/Parser.java
@@ -172,7 +172,8 @@ public class Parser {
 
         // Early check for decimals using regex
         if (param.matches(".*(mc/\\d+\\.\\d+|y/\\d+\\.\\d+|s/\\d+\\.\\d+).*")) {
-            throw new CEGStudyBuddyException("Invalid input: MC, year, and semester must be whole numbers, not decimals.");
+            throw new CEGStudyBuddyException("Invalid input: MC, year, and semester"
+                    + "must be whole numbers, not decimals.");
         }
 
         Pattern pattern = Pattern.compile(
